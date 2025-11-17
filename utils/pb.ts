74da34a6bee032eebfd65232351c63febcfb093c
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-11-17 21:57:13 +0100

    refactor: update PocketBase initialization and URL handling

--- utils/pb.ts
+++ utils/pb.ts
@@ -1,11 +1,8 @@
-import PocketBase from "pocketbase";
+import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
-
-const PB_URL = import.meta.env.PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090";
-
-const pb = new PocketBase(PB_URL) as TypedPocketBase;
-
-console.log("🔗 PocketBase URL:", PB_URL);
-
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    
+else path = 'https://portfolio.eloishenry.fr:443'   
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
-export { PB_URL as POCKETBASE_URL };
